67d572d32d9f23af85345b2794f78a6cf2b2b134
Mufti Ramdhani <muftiramdhani25@gmail.com>
2022-06-20 09:12:34 +0300

    coroutine flow

--- app/src/main/java/com/synrgy/tourismapp/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/synrgy/tourismapp/favorite/FavoriteViewModel.kt
@@ -1,9 +1,10 @@
 package com.synrgy.tourismapp.favorite
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.synrgy.tourismapp.core.domain.usecase.TourismUseCase
 
 class FavoriteViewModel(tourismUseCase: TourismUseCase) : ViewModel() {
-    val favoriteTourism = tourismUseCase.getFavoriteTourism()
+    val favoriteTourism = tourismUseCase.getFavoriteTourism().asLiveData()
 
 }
--- app/src/main/java/com/synrgy/tourismapp/home/HomeViewModel.kt
+++ app/src/main/java/com/synrgy/tourismapp/home/HomeViewModel.kt
@@ -1,8 +1,9 @@
 package com.synrgy.tourismapp.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.synrgy.tourismapp.core.domain.usecase.TourismUseCase
 
 class HomeViewModel(tourismUseCase: TourismUseCase) : ViewModel() {
-    val tourism = tourismUseCase.getAllTourism()
+    val tourism = tourismUseCase.getAllTourism().asLiveData()
 }
